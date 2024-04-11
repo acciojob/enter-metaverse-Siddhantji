99a07d8bb926ffe131302bcd649ace41d3b36e72
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-04-11 13:44:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const stat = document.getElementById("status");
 const btn = document.getElementById("enterBtn");
 btn.addEventListener("click",function(){
 	let heading = document.createElement("h1");
+	heading.id="status";
 	heading.textContent= "Entered Metaverse";
 	stat.parentNode.replaceChild(heading,stat);
 })
