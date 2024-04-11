b0a12ff25353be24fcdfcee10fce0bdadadd4074
Siddhant Teotia <91278902+Siddhantji@users.noreply.github.com>
2024-04-11 14:24:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-//your JS code here. If required.
-const stat = document.getElementById("status");
+
+
+let stat = document.getElementById("status");
 const btn = document.getElementById("enterBtn");
+
 btn.addEventListener("click",function(){
-	let heading = document.createElement("h1");
-	heading.id="status";
-	heading.textContent= "Entered Metaverse";
-	stat.parentNode.replaceChild(heading,stat);
-	
+    let heading = document.createElement("h1");
+    heading.id="status";
+    heading.textContent= "Entered Metaverse";
+    stat.parentNode.replaceChild(heading,stat);
+    stat = heading;
 })
